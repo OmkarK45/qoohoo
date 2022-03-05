19998ccb7d72759fdc1a20e87098e9a3d6c9acf0
Omkar <omkarak45@gmail.com>
2022-03-05 14:37:03 +0530

    feat: create Animation context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
+import { AnimationProvider } from '../hooks/useAnimation'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<AnimatePresence>
+			<AnimationProvider>
+				<Component {...pageProps} />
+			</AnimationProvider>
+		</AnimatePresence>
+	)
 }
 
 export default MyApp
