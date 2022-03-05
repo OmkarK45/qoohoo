dba72ae7b0a60457587ba7fd166d2e44f625c266
Omkar <omkarak45@gmail.com>
2022-03-05 14:36:21 +0530

    ui: refactor hero component styles

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image'
 import Blob2, { Blob1 } from '../Blobs'
 import { ProductCard } from '../Product/ProductCard'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { ProductTitle } from '../Product/ProductTitle'
+import { ProductDetailsButton } from '../Product/ProductDetailsButton'
 
 export function Hero() {
 	return (
 		<div className="flex flex-col h-full relative">
 			<div className="container mx-auto h-full flex flex-wrap ">
-				<div className="md:w-3/4 w-full pb-6 md:pb-0 md:pr-6  flex items-center">
-					<div className="flex flex-col flex-1 ml-24 justify-between  items-center">
-						<div className="font-bold tracking-tight text-6xl">
-							<h1>Tied green</h1>
-							<h1>V Neck Tshirt</h1>
+				<div className="md:w-5/6 w-full pb-6 md:pb-0 md:pr-6 flex items-center">
+					<div className="flex flex-col flex-1 ml-24 justify-between items-center">
+						<div className="flex items-center relative">
+							<div className="font-bold tracking-tight text-6xl">
+								<ProductTitle />
+							</div>
+							<div className="absolute -right-20">
+								<ProductDetailsButton />
+							</div>
 						</div>
 					</div>
 
-					<div className="z-[-1] h-full flex-1 flex items-center justify-center">
+					<div className="z-[-1] h-full flex-1 flex items-end justify-end">
 						<span className="relative">
 							<Image
 								src={'/assets/images/model1.png'}
@@ -34,7 +41,7 @@ export function Hero() {
 						</div>
 					</div>
 				</div>
-				<div className="md:w-1/4 w-full flex flex-col justify-center">
+				<div className="md:w-1/6 w-full flex flex-col justify-center">
 					<div className="flex flex-col space-y-6 max-w-[200px] mx-auto">
 						<ProductCard
 							image="/assets/images/handbag.png"
