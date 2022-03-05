26391e7cb171de82931c169b0bc3ad57605b21f8
Omkar <omkarak45@gmail.com>
2022-03-05 14:37:29 +0530

    feat: animate plus button

---
+++ src/components/Product/ProductDetailsButton.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion'
+import { AiOutlinePlus } from 'react-icons/ai'
+
+import { useAnimation } from '../../hooks/useAnimation'
+
+export function ProductDetailsButton() {
+	const { isAnimating, setIsAnimating } = useAnimation()
+
+	return (
+		<motion.button
+			onClick={() => setIsAnimating(!isAnimating)}
+			className="w-10 h-10 rounded-full bg-black"
+			initial={{ opacity: 0 }}
+			transition={isAnimating ? { duration: 1, delay: 0.3 } : { duration: 0.1 }}
+			animate={isAnimating ? { opacity: 1 } : { opacity: 0 }}
+			exit={{ opacity: 0 }}
+		>
+			<AiOutlinePlus className="w-full text-white" />
+		</motion.button>
+	)
+}
