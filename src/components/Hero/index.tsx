5ca9e5daf69ed1333a4fbbe0924f16338fd340f4
Omkar <omkarak45@gmail.com>
2022-03-05 19:18:03 +0530

    refactor: componentized model hero image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,62 +1,64 @@
-import Image from 'next/image'
-import Blob2, { Blob1 } from '../Blobs'
+import { AnimatePresence, motion } from 'framer-motion'
+
 import { ProductCard } from '../Product/ProductCard'
-import { AiOutlinePlus } from 'react-icons/ai'
 import { ProductTitle } from '../Product/ProductTitle'
+import { ProductDetails } from '../Product/ProductDetails'
 import { ProductDetailsButton } from '../Product/ProductDetailsButton'
 
+import { Blobs } from './Blobs'
+import { ModelImage } from './ModelImage'
+import { useAnimation } from '../../hooks/useAnimation'
+
 export function Hero() {
+	const { isAnimating } = useAnimation()
+
 	return (
 		<div className="flex flex-col h-full relative">
 			<div className="container mx-auto h-full flex flex-wrap ">
-				<div className="md:w-5/6 w-full pb-6 md:pb-0 md:pr-6 flex items-center">
-					<div className="flex flex-col flex-1 ml-24 justify-between items-center">
-						<div className="flex items-center relative">
-							<div className="font-bold tracking-tight text-6xl">
-								<ProductTitle />
-							</div>
-							<div className="absolute -right-20">
-								<ProductDetailsButton />
-							</div>
-						</div>
+				<div className="md:w-5/6 w-full pb-6 md:pb-0 md:pr-6 flex items-center ">
+					<div className="flex flex-col flex-1 ml-24 justify-between">
+						<AnimatePresence>
+							<motion.div layout={true} className="flex items-center relative">
+								<div>
+									<ProductTitle />
+									<ProductDetails />
+								</div>
+								<div className="absolute -right-20">
+									<ProductDetailsButton />
+								</div>
+							</motion.div>
+						</AnimatePresence>
 					</div>
 
-					<div className="z-[-1] h-full flex-1 flex items-end justify-end">
-						<span className="relative">
-							<Image
-								src={'/assets/images/model1.png'}
-								width={600}
-								height={1200}
-								alt="Model wearning a long jacket and a hat"
+					<ModelImage />
+
+					<Blobs />
+				</div>
+
+				{isAnimating ? (
+					<div className="md:w-1/6 w-full flex flex-col justify-center">
+						<motion.div
+							initial={{ y: 500, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{ duration: 1 }}
+							exit={{ y: -500, opacity: 0 }}
+							className="flex flex-col space-y-6 max-w-[200px] mx-auto"
+						>
+							<ProductCard
+								image="/assets/images/handbag.png"
+								price={24}
+								title="LV Handbag"
+								imageAlt="Image of handbag"
 							/>
-						</span>
+							<ProductCard
+								image="/assets/images/handbag2.png"
+								price={24}
+								title="Not LV Handbag"
+								imageAlt="Image of handbag"
+							/>
+						</motion.div>
 					</div>
-
-					<div className="absolute z-[-2] inset-0 h-full flex-1 flex items-center justify-center">
-						<div className="absolute h-full z-[-1]">
-							<Blob1 className="h-full opacity-50" />
-						</div>
-						<div className="absolute h-full z-[-1]">
-							<Blob2 className="h-full" />
-						</div>
-					</div>
-				</div>
-				<div className="md:w-1/6 w-full flex flex-col justify-center">
-					<div className="flex flex-col space-y-6 max-w-[200px] mx-auto">
-						<ProductCard
-							image="/assets/images/handbag.png"
-							price={24}
-							title="LV Handbag"
-							imageAlt="Image of handbag"
-						/>
-						<ProductCard
-							image="/assets/images/handbag2.png"
-							price={24}
-							title="Not LV Handbag"
-							imageAlt="Image of handbag"
-						/>
-					</div>
-				</div>
+				) : null}
 			</div>
 		</div>
 	)
