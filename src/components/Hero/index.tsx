8612ab634adc7b6189f0c911e036c38b7562a74e
Omkar <omkarak45@gmail.com>
2022-03-05 13:55:43 +0530

    ui: added blobs on hero page
    * added absolutely positioned bg blobs

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
+import Blob2, { Blob1 } from '../Blobs'
 import { ProductCard } from '../Product/ProductCard'
 
 export function Hero() {
 	return (
-		<div className="flex flex-col h-full relative ">
-			<div className="absolute z-[-1] top-20 left-0 h-full">Blobs</div>
-
+		<div className="flex flex-col h-full relative">
 			<div className="container mx-auto h-full flex flex-wrap ">
 				<div className="md:w-3/4 w-full pb-6 md:pb-0 md:pr-6  flex items-center">
 					<div className="flex flex-col flex-1 ml-24 justify-between  items-center">
@@ -14,7 +13,8 @@ export function Hero() {
 							<h1>V Neck Tshirt</h1>
 						</div>
 					</div>
-					<div className=" z-[-1] h-full flex-1 flex items-center justify-center">
+
+					<div className="z-[-1] h-full flex-1 flex items-center justify-center">
 						<span className="relative">
 							<Image
 								src={'/assets/images/model1.png'}
@@ -23,6 +23,15 @@ export function Hero() {
 								alt="Model wearning a long jacket and a hat"
 							/>
 						</span>
+					</div>
+
+					<div className="absolute z-[-2] inset-0 h-full flex-1 flex items-center justify-center">
+						<div className="absolute h-full z-[-1]">
+							<Blob1 className="h-full opacity-50" />
+						</div>
+						<div className="absolute h-full z-[-1]">
+							<Blob2 className="h-full" />
+						</div>
 					</div>
 				</div>
 				<div className="md:w-1/4 w-full flex flex-col justify-center">
