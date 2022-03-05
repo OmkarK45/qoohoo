38b0a0d8d00c468d4a8b3f3f5c6767f3a4ece449
Omkar <omkarak45@gmail.com>
2022-03-05 13:18:40 +0530

    init: initial commit
    * Scaffold  project using create-next-app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('tailwindcss/tailwind-config').TailwindConfig }
+ **/
+module.exports = {
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
