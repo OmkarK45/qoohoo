ca7e208647d09d22dad763f0b324b1cf9c96f6ec
Omkar <omkarak45@gmail.com>
2022-03-05 13:56:19 +0530

    ui: adjusted layout
    * removed padding that caused overflow
    * added dummy footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,12 @@ import { Navbar } from '../Navbar/Navbar'
 
 export function PageLayout() {
 	return (
-		<div className="container mx-auto ">
-			<div className="flex flex-wrap min-h-screen">
-				<div className="md:w-3/4 w-full pb-6 md:pb-0 md:pr-6">
+		<div className="container mx-auto">
+			<div className="flex flex-wrap">
+				<div className="md:w-3/4 w-full md:pr-6">
 					<Navbar />
 					<Hero />
+					Footer
 				</div>
 				<div className="md:w-1/4 w-full">
 					<div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24" />
